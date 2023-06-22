a88c2bef23cd65257b265219cefd14530a477b4f
Jesus <vargas0073322gmail.com>
2023-06-21 22:32:37 -0500

    first commit

---
+++ Cita.dart
@@ -0,0 +1,28 @@
+/*
+class Cita {
+  String? _cita1;
+  String? _cita2;
+  String? _cita3;
+
+  String get cita1() {
+    return cita1;
+  }
+  set cita1(String ct1) {
+    this.cita1 = ct1;
+  }
+
+  String get cita2() {
+    return cita2;
+  }
+  set cita2(String ct2) {
+    this.cita2 = ct2;    
+  }
+
+  String get cita3() {
+    return cita3;
+  }
+  set cita3(String ct3) {
+    this.cita3 = ct3;
+  }
+}
+*/
---
+++ Sistema.dart
@@ -0,0 +1,94 @@
+import 'Cita.dart';
+import 'dart:io';
+
+main() {
+  print('Agendamiento de citas medicas.');
+  print('- cita1');
+  print('- cita2');
+  print('- cita3');
+  print('Escriba una de las citas que desea seleccionar:');
+  String seleccion = stdin.readLineSync().toString();
+
+  while (seleccion == '') {
+    print('Escriba una de las citas que desea seleccionar.');
+    print('- cita1');
+    print('- cita2');
+    print('- cita3');
+    print('Seleccione una y escribala.');
+    seleccion = stdin.readLineSync().toString();
+  }
+
+  while (seleccion != 'cita1' && seleccion != 'cita2' && seleccion != 'cita3') {
+    print(
+        'Si quiere seleccionar alguna de las opciones tiene que escribirlas igual como se muestran.');
+    seleccion = stdin.readLineSync().toString();
+  }
+
+  stdout.writeln('Su seleccion ha sido esta: $seleccion');
+
+  if (seleccion == 'cita1' && seleccion != 'cita2' && seleccion != 'cita3') {
+    Citas();
+  }
+/*
+  print('SOLICITUD DE CITA.');
+  print('Cita seleccionada: $seleccion');
+  print('DATOS DE LA CITA');
+  //print('Fecha: $fecha');
+*/
+}
+
+void Citas() {
+  String fecha;
+  String hora;
+  String direccion;
+
+  print('Ingrese los datos para apartar la cita.');
+  print('Ingrese la fecha de cita Ej: DD/MM/AA');
+  fecha = stdin.readLineSync().toString();
+  while (fecha == '') {
+    print(
+        'Debe ingresar una fecha para poder continuar con la solicitud de cita. Ej: DD/MM/AA');
+    fecha = stdin.readLineSync().toString();
+  }
+  print('Ingrese la hora de cita Ej:(6:00, 5:30 o 5:25): ');
+  hora = stdin.readLineSync().toString();
+  while (hora == '') {
+    print('Debe ingresar la hora en la que quiere diligenciar la cita:');
+    hora = stdin.readLineSync().toString();
+  }
+  print('Ingrese la direccion:');
+  direccion = stdin.readLineSync().toString();
+  while (direccion == '') {
+    print('Ingresar una direccion:');
+    direccion = stdin.readLineSync().toString();
+  }
+}
+
+mostrarDatos() {}
+
+
+
+
+
+
+/*
+import 'dart:io';
+
+class Cita {
+  String fecha;
+  String hora;
+  String direccion;
+}
+
+main() {
+  final cita1 = new Cita();
+
+  Map<String, String> cita = {
+    'Fecha': '',
+    'Hora': '',
+    'Direccion': ''
+  };
+
+  stdin.readLineSync()  
+}
+*/
